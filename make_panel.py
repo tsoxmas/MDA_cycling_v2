6ef06c85e16ea30ccce58c67d651f478f9724cf0
tsoxmas <catslover0@gmail.com>
2026-07-05 17:13:21 +0200

    added imputation pipeline with a simple imputer (replace na w/ median)

--- make_panel.py
+++ make_panel.py
@@ -4,13 +4,15 @@ import numpy as np
 import pandas as pd
 import holidays
 import paths as p
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
 
 bike = pd.read_parquet(p.data_file, columns=p.cols)
 
 bike["ts"] = pd.to_datetime(bike["ts"], utc=True, errors="coerce")
-bike = bike.dropna(subset=["ts", "site_id", "count"]) # removing rows w/ missing data
-local_time = bike["ts"].dt.tz_convert(p.local_tz) # converting time to BElgian time (tz = "Europe/Brussels")
-bike["date"] = local_time.dt.tz_localize(None).dt.normalize()  #  calendar day
+bike = bike.dropna(subset=["ts", "site_id", "count"])  # removing rows w/ missing data
+local_time = bike["ts"].dt.tz_convert(p.local_tz)  # converting time to BElgian time (tz = "Europe/Brussels")
+bike["date"] = local_time.dt.tz_localize(None).dt.normalize()  # calendar day
 bike["hour"] = local_time.dt.hour  # local hour
 bike["dow"] = local_time.dt.dayofweek  # Monday = 0, Sunday = 6
 bike["month"] = local_time.dt.month  # month
@@ -22,14 +24,14 @@ bike["commute_count"] = bike["count"] * bike["commute"]  # count cyclists during
 
 # adding one row per day for each site
 daily = bike.groupby(["site_id", "date"]).agg(
-    total_count=("count", "sum"),  #daily cycling volume
+    total_count=("count", "sum"),  # daily cycling volume
     n_slots=("count", "size"),  # how many 15-minute rows were present
     commute_count=("commute_count", "sum"),  # cyclists count during commute hours
     precip_mm=("precip_quantity", "sum"),  # daily rain amount
     rainy_slots=("is_rain", "sum"),  # number of rain intervals
     heavy_slots=("is_heavy_rain", "sum"),  # number of heavy rain intervals
     temp_mean=("temp_dry_shelter_avg", "mean"),  # daily average temperature
-    wind_mean=("wind_speed_10m", "mean"),  #daily average wind speed
+    wind_mean=("wind_speed_10m", "mean"),  # daily average wind speed
     gust_max=("wind_gusts_speed", "max"),  # maximum gust that day
     hum_mean=("humidity_rel_shelter_avg", "mean"),  # daily average humidity
     pressure_mean=("pressure", "mean"),  # daily average atmosphetic pressure
@@ -42,6 +44,13 @@ daily = bike.groupby(["site_id", "date"]).agg(
     month=("month", "first"),  # ьщтер
     weekend=("weekend", "first"),  # weekend flag
 ).reset_index()
+
+print(daily["wind_mean"].isna().mean())
+print(daily["wind_mean"].describe())
+numeric_cols = ["precip_mm", "temp_mean", "wind_mean", "gust_max", "hum_mean", "pressure_mean", "sun_min",
+                "radiation_mean", "station_km"]
+imputation = Pipeline([("imputer", SimpleImputer(strategy="median")), ])
+daily[numeric_cols] = imputation.fit_transform(daily[numeric_cols])
 
 daily["rain_day"] = (daily["precip_mm"].fillna(0) > 0).astype(int)  # day with any amount of rain dummy
 daily["heavy_rain_day"] = (daily["precip_mm"].fillna(0) >= 5).astype(int)  # day with a heavy-ish amount of rain dummy
